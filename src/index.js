82701adab15f0747b169bc6611849708cf398b65
Yuki Nagae <yuki.nagae1130@gmail.com>
2020-05-20 13:20:12 +0900

    HR will work for index.js/index.html/style.css

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import './index.html';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = 'Hello from index.js';
+  element.classList.add('hello');
+
+  return element;
+}
   
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+document.body.appendChild(component());
 
 if (module.hot) {
-  module.hot.accept('./', function() {
+  module.hot.accept('./index.js', function() {
     console.log('Accepting the updated index.js module!');
   })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
     hot: true,
   },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [    
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "raw-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
   },
 };
